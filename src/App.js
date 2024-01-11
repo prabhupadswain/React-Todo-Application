259d9405c5cd4f4d0e09f86d148b2d9dbd9c4a07
prabhupadswain <prabhupad.swain@outlook.com>
2024-01-11 14:55:04 +0530

    App component function converted to arrow function & react imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import Todo from './components/Todo';
+import React from 'react';
 
-function App() {
+import Todo from './components/Todo.js';
+
+const App = () => {
   return (
-  <div>
+    <div>
       <h1>My Todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Master React'/>
-      <Todo text='Learn full React'/>
-  </div>
+      <Todo text='Learn react' />
+      <Todo text='Learn advanced react' />
+      <Todo text='Master react' />
+      <Todo text='Build projects in react' />
+    </div>
   );
-}
+};
 
 export default App;
